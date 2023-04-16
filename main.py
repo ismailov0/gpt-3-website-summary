2acff372387c47da62cdba791db4dd67c642420c
Ismailov Arsen <aismailov0205@gmail.com>
2023-04-17 02:30:48 +0600

    Updated to PEP8

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+"""
+Модуль содержит класс WebSiteAssistant, который позволяет создавать ассистента,
+способного изучать текст веб-страниц с целью составить краткое резюме
+из основных идей на русском языке.
+
+Класс содержит следующие методы:
+
+    - __init__(self, url: str) -> None: конструктор класса Assistant, 
+        принимающий URL-адрес веб-страницы.
+    - summary(self) -> str: метод для создания краткого резюме из основных идей веб-страницы.
+"""
 import sys
 import os
 import openai
@@ -7,6 +18,7 @@ from bs4 import BeautifulSoup
 
 
 load_dotenv()
+
 # Ваш API KEY от OpenAI.
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -15,7 +27,8 @@ DEFAULT_URL = 'https://kino.kz/'
 
 class WebSiteAssistant:
     """
-    Класс для создания ассистента, который изучает текст с целью составить краткое резюме из его основных идей на русском языке.
+    Класс для создания ассистента, который изучает текст с целью \
+        составить краткое резюме из его основных идей на русском языке.
     """
 
     def __init__(self, url: str) -> None:
@@ -43,7 +56,8 @@ class WebSiteAssistant:
         """
         messages = [
             {
-                "role": "system", "content": "Ты - асистент, который изучает текст с целью составить краткое резюме из его основных идей на русском языке"
+                "role": "system", "content": "Ты - асистент, который изучает текст с целью' \
+                      ' составить краткое резюме из его основных идей на русском языке"
             },
         ]
         messages.append({"role": "user", "content": cleaned_html_text})
@@ -56,10 +70,13 @@ class WebSiteAssistant:
         Внутренний метод для получения HTML-кода веб-страницы.
         """
         headers = {
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '
+            'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9'
+            ',image/avif,image/webp,image/apng,*/*;q=0.8,'
+            'application/signed-exchange;v=b3;q=0.7'
         }
-        response = requests.get(self.url, headers=headers)
+        response = requests.get(self.url, headers=headers, timeout=10)
         return response.content
 
     def _parse_data(self, html: str) -> str:
@@ -78,10 +95,10 @@ class WebSiteAssistant:
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        url = sys.argv[1]
-        if not url.startswith('http'):
-            url = 'https://' + url
+        URL = sys.argv[1]
+        if not URL.startswith('http'):
+            URL = 'https://' + URL
     else:
-        url = DEFAULT_URL
-    assistant = WebSiteAssistant(url)
+        URL = DEFAULT_URL
+    assistant = WebSiteAssistant(URL)
     print(assistant.summary)
